926fbea7f15d1a134c5fb3042bde0ab85872a1a4
Rasmus Klintsø <rall1812@gmail.com>
2023-03-15 12:55:38 +0100

    wip decorator

---
+++ Assets/Scripts/BehaviorTree/Decorator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+    public class Decorator : Node
+    {
+        private Node _child;
+
+        public Decorator() : base()
+        {
+        }
+
+        public Decorator(List<Node> children) : base(children)
+        {
+            _child = children[0];
+        }
+    
+        public Decorator(Node child)
+        {
+            _child = child;
+        }
+
+        
+        public override NodeState Evaluate()
+        {
+            return state;
+        }
+    }
+    
+    
+}
