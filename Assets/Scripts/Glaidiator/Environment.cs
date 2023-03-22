fddb351b5d087b043c789d6482d6f201a43c6b0f
Rasmus Klintsø <rall1812@gmail.com>
2023-03-22 13:47:56 +0100

    wip collisions, added ColBox class

--- Assets/Scripts/Glaidiator/Environment.cs
+++ Assets/Scripts/Glaidiator/Environment.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Glaidiator.Model;
 using UnityEngine;
 
 namespace Glaidiator
@@ -12,19 +13,21 @@ namespace Glaidiator
         public static double width;
         public static double depth;
 
-        public static Model.Character player;
-        public static Model.Character boss;
+        public static Character player;
+        public static Character boss;
     
-        private Dictionary<Model.Character, Vector2> positions;
-        private Dictionary<Vector2, Model.Character> characters;
-    
-    
+        private Dictionary<Character, Vector2> positions;
+        private Dictionary<Vector2, Character> characters;
+
+        private List<ColBox> cols;
+
 
         private void Awake()
         {
             instance = this;
-            positions = new Dictionary<Model.Character, Vector2>();
-            characters = new Dictionary<Vector2, Model.Character>();
+            positions = new Dictionary<Character, Vector2>();
+            characters = new Dictionary<Vector2, Character>();
+            cols = new List<ColBox>();
 
             player.onMove += OnMove;
             boss.onMove += OnMove;
@@ -44,18 +47,22 @@ namespace Glaidiator
     
         void Start()
         {
-        
+            if (player != null) cols.Add(new ColBox(player, ColBox.ColType.Body));
+            if (boss != null) cols.Add(new ColBox(boss, ColBox.ColType.Body));
         }
 
-    
         void Update()
         {
-        
+            // TODO: include the deltaTime tick scaling
+            
+            // check collisions
+            foreach(ColBox col in cols)
+            {
+                col.Update(Time.deltaTime);
+                
+            }
         }
-
     
-        //public static Vector2 ConvertToModel(Vector3 )
-
         public static bool IsIntersectOther(Model.Character agent1)
         {
             Model.Character agent2;
@@ -71,9 +78,63 @@ namespace Glaidiator
             Vector2 origin = new Vector2(player.Movement.Position.x,
                 player.Movement.Position.z);
         
-        
-        
+            
             return false;
         }
+    }
+
+    
+    public class ColBox
+    {
+        private Character _owner;
+        private ColType _colType;
+        private float _duration;
+
+        private Vector2 _origin;
+
+        private Vector2 corner1;
+        private Vector2 corner2;
+
+        private float offset = 0.75f; // radius of character footprint 2D topdown size
+        private Vector2 offset2d = new Vector2(0.75f, 0.75f);
+
+        public enum ColType
+        {
+            Body = 0,
+            Attack = 1,
+            //Defense = 2
+        }
+        
+        public ColBox(Character owner, ColType colType)
+        {
+            _owner = owner;
+            _colType = colType;
+            _origin = new Vector2(_owner.Movement.Position.x, _owner.Movement.Position.z);
+            corner1 = _origin - offset2d;
+            corner2 = _origin + offset2d;
+        }
+
+        public ColBox(Character owner, ColType colType, float duration)
+        {
+            _owner = owner;
+            _colType = colType;
+            _duration = duration;
+        }
+
+        public void Update(float dTime)
+        {
+            _duration -= dTime;
+            if (_colType != ColType.Body && _duration < 0f)
+            {
+                // remove box
+            }
+            
+            _origin = new Vector2(_owner.Movement.Position.x, _owner.Movement.Position.z);
+            corner1 = _origin - offset2d;
+            corner2 = _origin + offset2d;
+
+
+        }
+        
     }
 }
