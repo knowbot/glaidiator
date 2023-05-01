9b343e2792219a3f4fde61011fd5c1c5c59239ee
Rasmus Klintsø <rall1812@gmail.com>
2023-05-01 13:45:49 +0200

    added tasks FaceEnemy + CheckEnemyDistance

---
+++ Assets/Scripts/BasicAI/CheckEnemyDistance.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using BehaviorTree;
+using Glaidiator.Model;
+
+namespace BasicAI
+{
+    public class CheckEnemyDistance : Node
+    {
+
+        private float _threshold;
+        
+        public CheckEnemyDistance(float threshold)
+        {
+            _threshold = threshold;
+        }
+
+        public override NodeState Evaluate()
+        {
+            if (tree == null) throw new NullReferenceException();
+        
+            Movement target = (Movement)GetData("target");
+            if (target == null)
+            {
+                state = NodeState.FAILURE;
+                return state;
+            }
+
+            if (Vector3.Distance(_ownerCharacter.Movement.Position, target.Position) <= _threshold)
+            {
+                state = NodeState.SUCCESS;
+            }
+            else
+            {
+                state = NodeState.FAILURE;
+            }
+            
+            return state;
+        }
+        
+        
+        public override Node Clone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Mutate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Node Randomized()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/BasicAI/TaskFaceEnemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using BehaviorTree;
+using Glaidiator.Model;
+using UnityEngine;
+
+namespace BasicAI
+{
+    public class TaskFaceEnemy : Node
+    {
+
+        public TaskFaceEnemy()
+        {
+            
+        }
+
+        public override NodeState Evaluate()
+        {
+            if (tree == null) throw new NullReferenceException();
+        
+            Movement target = (Movement)GetData("target");
+            if (target == null)
+            {
+                state = NodeState.FAILURE;
+                return state;
+            }
+
+            Vector3 myPos = _ownerCharacter.Movement.Position;
+            tree.Direction = (target.Position - myPos).normalized;
+
+            state = NodeState.SUCCESS;
+            return state;
+        }
+
+
+        public override Node Clone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Mutate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Node Randomized()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
