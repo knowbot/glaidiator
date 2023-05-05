958a82509f547ae9b453e11cf04c5b18f2b4f8da
Andrea Trevisin <andre.trevisin@gmail.com>
2023-05-05 18:34:14 +0200

    LFS stuff + CHECK CAN DO ACTION NODE POG

---
+++ Assets/Scripts/BasicAI/CheckCanDoAction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using BehaviorTree;
+using Glaidiator.Model;
+using Glaidiator.Model.Actions;
+
+namespace BasicAI
+{
+    public class CheckEnemyAction : Node
+    {
+
+        private string _actionName;
+        
+        public CheckEnemyAction(string actionName)
+        {
+            _actionName = actionName;
+        }
+
+        public override NodeState Evaluate()
+        {
+            tree.currentNode = this;
+            if (tree == null) throw new NullReferenceException();
+        
+            IAction action = ((Character)GetData("enemy"))?.ActiveAction;
+            if (action == null)
+            {
+                state = NodeState.FAILURE;
+                return state;
+            }
+
+            state = _actionName == action.Action.Name ? NodeState.SUCCESS : NodeState.FAILURE;
+            
+            return state;
+        }
+        
+        
+        public override Node Clone()
+        {
+            return new CheckEnemyAction(_actionName);
+        }
+
+        public override void Mutate()
+        {
+            return;
+        }
+
+        public override Node Randomized()
+        {
+            return new CheckEnemyAction(_actionName);
+        }
+    }
+}
